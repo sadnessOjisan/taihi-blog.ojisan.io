61034e255f97c722f5cd5a715b9bd52af343df53
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-07 20:25:34 +0900

    chore: use ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,3 +8,17 @@ const Root = () => {
 };
 
 export default Root;
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  //   const res = await fetch("https://.../posts");
+  //   const posts = await res.json();
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts: [],
+    },
+  };
+}

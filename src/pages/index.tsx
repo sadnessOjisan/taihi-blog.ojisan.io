1a2a1a60cdce75c20c5a718ff4b87eff9d4fabaf
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-07 21:05:36 +0900

    feat: load contents data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
-const Root = () => {
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const Root = (props) => {
+  console.log(props);
   return (
     <div>
       <h1>blog.ojisan.io</h1>
@@ -10,15 +15,23 @@ const Root = () => {
 export default Root;
 
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts
-  //   const res = await fetch("https://.../posts");
-  //   const posts = await res.json();
+  const files = fs.readdirSync(path.join("src/contents"));
+  const posts = files.map((slug) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join("src", "contents", slug, "index.md"),
+      "utf-8"
+    );
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
   return {
     props: {
-      posts: [],
+      posts,
     },
   };
 }

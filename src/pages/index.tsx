eecfa89f3ffcd4c9a6ddf4a4948b84c0571de419
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-07 21:09:35 +0900

    chore: show frontmatter data to top page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,40 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { GetStaticProps } from "next";
+import { VFC } from "react";
 
-const Root = (props) => {
-  console.log(props);
+type Props = {
+  posts: {
+    slug: string;
+    frontmatter: {
+      [key: string]: any;
+    };
+  }[];
+};
+
+const Root: VFC<Props> = (props) => {
   return (
     <div>
       <h1>blog.ojisan.io</h1>
-      <div>TODO: contents</div>
+      <div>
+        {props.posts.map((p) => (
+          <div>
+            <a href={p.slug}>
+              <div>
+                <p>{JSON.stringify(p.frontmatter)}</p>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Root;
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps<Props> = () => {
   const files = fs.readdirSync(path.join("src/contents"));
   const posts = files.map((slug) => {
     const markdownWithMeta = fs.readFileSync(
@@ -34,4 +54,4 @@ export async function getStaticProps() {
       posts,
     },
   };
-}
+};

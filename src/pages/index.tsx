8ad8c54c190077cf4eb2170a5b8031cb77149da1
Yuta Ide <sadness.ojisan@gmail.com>
2021-09-08 01:34:55 +0900

    Add contents data (#3)
    * feat: add blog contents
    
    * feat: load contents data
    
    * chore: show frontmatter data to top page
    
    * fix: lint error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,57 @@
-const Root = () => {
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { GetStaticProps } from "next";
+import { VFC } from "react";
+
+type Props = {
+  posts: {
+    slug: string;
+    frontmatter: {
+      [key: string]: any;
+    };
+  }[];
+};
+
+const Root: VFC<Props> = (props) => {
   return (
     <div>
       <h1>blog.ojisan.io</h1>
-      <div>TODO: contents</div>
+      <div>
+        {props.posts.map((p) => (
+          <div key={p.slug}>
+            <a href={p.slug}>
+              <div>
+                <p>{JSON.stringify(p.frontmatter)}</p>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Root;
 
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts
-  //   const res = await fetch("https://.../posts");
-  //   const posts = await res.json();
+export const getStaticProps: GetStaticProps<Props> = () => {
+  const files = fs.readdirSync(path.join("src/contents"));
+  const posts = files.map((slug) => {
+    const markdownWithMeta = fs.readFileSync(
+      path.join("src", "contents", slug, "index.md"),
+      "utf-8"
+    );
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
   return {
     props: {
-      posts: [],
+      posts,
     },
   };
-}
+};

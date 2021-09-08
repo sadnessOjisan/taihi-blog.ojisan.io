a8d62550a15c60a9f84a5eecf3f60ada312d68f4
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-08 14:23:10 +0900

    feat: add ga track id

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,8 +56,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-119542494-2", // Google Analytics / GA
+        ],
+      },
+    },
   ],
 };

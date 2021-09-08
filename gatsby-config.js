3e9408bcce6ac3f30fc651550d9c91bb1b81c617
Yuta Ide <sadness.ojisan@gmail.com>
2021-09-08 14:09:39 +0900

    ReImpl by gatsby (#5)
    * feat: use gatsby
    
    * remove: unused files
    
    * chore: show image
    
    * feat: impl ogp
    
    * fix: hide image
    
    * Fix: format
    
    * chore: update script

---
+++ gatsby-config.js
@@ -0,0 +1,63 @@
+module.exports = {
+  siteMetadata: {
+    title: `blog.ojisan.io`,
+    description: `ojsan blog.`,
+    author: `@sadnessOjisan`,
+    siteUrl: `https://blog.ojisan.io`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/src/contents`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              destinationDir: `path/to/dir`,
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
---
+++ src/pages/{MarkdownRemark.frontmatter__path}.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+// import Img from "gatsby-image";
+
+// FIXME: any
+export default function Template(props: any) {
+  const { markdownRemark } = props.data; // data.markdownRemark holds your post data
+  const { frontmatter, html, excerpt } = markdownRemark;
+  return (
+    <Layout>
+      <Seo title={frontmatter.title} description={excerpt} />
+      <div className="blog-post">
+        <h1>{frontmatter.title}</h1>
+        {/* <Img fluid={frontmatter.visual.childImageSharp.fluid} /> */}
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+      </div>
+    </Layout>
+  );
+}
+export const pageQuery = graphql`
+  query ($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        path
+        visual {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+      excerpt(pruneLength: 140)
+    }
+  }
+`;

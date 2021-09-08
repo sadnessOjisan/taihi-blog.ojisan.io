d429cb0c654f174c284642a3b6a5a3d4a09ed342
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-08 14:06:00 +0900

    fix: hide image

--- src/pages/{MarkdownRemark.frontmatter__path}.tsx
+++ src/pages/{MarkdownRemark.frontmatter__path}.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import Img from "gatsby-image";
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html, excerpt } = markdownRemark;
+  console.log(frontmatter.visual);
   return (
     <Layout>
       <Seo title={frontmatter.title} description={excerpt} />
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        {/* <Img fluid={frontmatter.visual.childImageSharp.fluid} /> */}
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
@@ -28,6 +30,13 @@ export const pageQuery = graphql`
       frontmatter {
         title
         path
+        visual {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
       }
       excerpt(pruneLength: 140)
     }
